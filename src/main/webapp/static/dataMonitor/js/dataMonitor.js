3f63ef706f454fb2211c3e2162e3c80c00b0ff0a
anke5156 <anke.wang@foxmail.com>
2018-09-09 15:58:52 +0800

    调整数据监测页面，去掉实时折线图

--- src/main/webapp/static/dataMonitor/js/dataMonitor.js
+++ src/main/webapp/static/dataMonitor/js/dataMonitor.js
@@ -177,79 +177,79 @@ option1 = {
 }
 myChart1.setOption(option1);
 
-
-
-var myChart2 = echarts.init(document.getElementById('myChart2s'));
-
-
-function randomData() {
-    now = new Date(+now + oneDay);
-    value = value + Math.random() * 21 - 10;
-    return {
-        name: now.toString(),
-        value: [
-            [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
-            Math.round(value)
-        ]
-    }
-}
-
-var data = [];
-var now = +new Date(2018, 6, 26);
-var oneDay = 3600 * 1000;
-var value = Math.random() * 1000;
-for (var i = 0; i < 1000; i++) {
-    data.push(randomData());
-}
-
-option2 = {
-    title: {
-        text: '实时指数走势'
-    },
-    tooltip: {
-        trigger: 'axis',
-        formatter: function (params) {
-            params = params[0];
-            var date = new Date(params.name);
-            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
-        },
-        axisPointer: {
-            animation: false
-        }
-    },
-    xAxis: {
-        type: 'time',
-        splitLine: {
-            show: false
-        }
-    },
-    yAxis: {
-        type: 'value',
-        boundaryGap: [0, '100%'],
-        splitLine: {
-            show: false
-        }
-    },
-    series: [{
-        name: '模拟数据',
-        type: 'line',
-        showSymbol: false,
-        hoverAnimation: false,
-        data: data
-    }]
-};
-
-setInterval(function () {
-
-    for (var i = 0; i < 5; i++) {
-        data.shift();
-        data.push(randomData());
-    }
-
-    myChart2.setOption({
-        series: [{
-            data: data
-        }]
-    });
-}, 1000);
-myChart2.setOption(option2);
+//
+//
+// var myChart2 = echarts.init(document.getElementById('myChart2s'));
+//
+//
+// function randomData() {
+//     now = new Date(+now + oneDay);
+//     value = value + Math.random() * 21 - 10;
+//     return {
+//         name: now.toString(),
+//         value: [
+//             [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+//             Math.round(value)
+//         ]
+//     }
+// }
+//
+// var data = [];
+// var now = +new Date(2018, 6, 26);
+// var oneDay = 3600 * 1000;
+// var value = Math.random() * 100;
+// for (var i = 0; i < 1000; i++) {
+//     data.push(randomData());
+// }
+//
+// option2 = {
+//     title: {
+//         text: '实时指数走势'
+//     },
+//     tooltip: {
+//         trigger: 'axis',
+//         formatter: function (params) {
+//             params = params[0];
+//             var date = new Date(params.name);
+//             return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+//         },
+//         axisPointer: {
+//             animation: false
+//         }
+//     },
+//     xAxis: {
+//         type: 'time',
+//         splitLine: {
+//             show: false
+//         }
+//     },
+//     yAxis: {
+//         type: 'value',
+//         boundaryGap: [0, '100%'],
+//         splitLine: {
+//             show: false
+//         }
+//     },
+//     series: [{
+//         name: '模拟数据',
+//         type: 'line',
+//         showSymbol: false,
+//         hoverAnimation: false,
+//         data: data
+//     }]
+// };
+//
+// setInterval(function () {
+//
+//     for (var i = 0; i < 5; i++) {
+//         data.shift();
+//         data.push(randomData());
+//     }
+//
+//     myChart2.setOption({
+//         series: [{
+//             data: data
+//         }]
+//     });
+// }, 1000);
+// myChart2.setOption(option2);
